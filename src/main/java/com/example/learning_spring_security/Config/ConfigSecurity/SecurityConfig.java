415f3ac2f5e83efdaa427e5850c1d3f422a5c765
senghour <seanghour097328@gmail.com>
2026-02-24 21:50:49 +0700

    seng hour

--- src/main/java/com/example/learning_spring_security/Config/ConfigSecurity/SecurityConfig.java
+++ src/main/java/com/example/learning_spring_security/Config/ConfigSecurity/SecurityConfig.java
@@ -127,11 +127,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000/"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
