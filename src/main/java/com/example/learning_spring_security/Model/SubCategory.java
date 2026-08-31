ca51224a08189268cf3b8f744c87ba6d66cb3774
senghour <seanghour097328@gmail.com>
2026-08-31 23:20:46 +0700

    add database

--- src/main/java/com/example/learning_spring_security/Model/SubCategory.java
+++ src/main/java/com/example/learning_spring_security/Model/SubCategory.java
@@ -25,7 +25,7 @@ public class SubCategory extends BaseEntity{
     @OneToOne(cascade = CascadeType.ALL)
     private Image image;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false,columnDefinition = "TEXT")
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
     @OneToMany(mappedBy = "subCategory", fetch = FetchType.LAZY)
